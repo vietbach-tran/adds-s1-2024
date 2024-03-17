65487eb22ad1db2e573fe3bf48384f0f22eb0230
Viet Bach Tran <a1901793@adelaide.edu.au>
2024-03-17 12:29:30 +1030

    prac3

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,10 +1,20 @@
 #include "EfficientTruckloads.h"
+#include <vector>
 
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize)
 {
+  std::vector<int> dp(numCrates, -1);
   if (numCrates == 0)
-    return 0;
+    return dp[0] == 0;
   if (numCrates <= loadSize)
-    return 1;
-  return numTrucks(int(numCrates / 2), loadSize) + numTrucks(numCrates - (numCrates / 2), loadSize);
+    return dp[numCrates] = 1;
+  if (dp[int(numCrates / 2)] == -1)
+  {
+    dp[int(numCrates / 2)] = numTrucks(int(numCrates / 2), loadSize);
+  }
+  if (dp[numCrates - int(numCrates / 2)] == -1)
+  {
+    dp[numCrates - int(numCrates / 2)] = numTrucks(numCrates - int(numCrates / 2), loadSize);
+  }
+  return dp[int(numCrates / 2)] + dp[numCrates - int(numCrates / 2)];
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "Truckloads.h"
 #include "Reverser.h"
+#include "EfficientTruckloads.h"
 #include <iostream>
 
 int main()
 {
   Truckloads t;
   Reverser r;
+  EfficientTruckloads e;
   // std::cout << t.numTrucks(1024, 5) << std::endl;
   std::cout << r.reverseDigit(12345) << std::endl;
   std::cout << r.reverseString("abcde") << std::endl;
+  std::cout << e.numTrucks(1024, 5) << std::endl;
 }
